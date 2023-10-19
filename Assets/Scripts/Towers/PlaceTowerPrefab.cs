578f42fab747bb6dac68f0a0921b6d0584d345a8
Fronter <mathijs.klingeman@gmail.com>
2023-10-19 23:01:18 +0200

    fixed bug where tower placeable on road or other towers

--- Assets/Scripts/Towers/PlaceTowerPrefab.cs
+++ Assets/Scripts/Towers/PlaceTowerPrefab.cs
@@ -9,6 +9,9 @@ public class PlaceTowerPrefab : MonoBehaviour
     private SpriteRenderer[] spriteRenderers;
     public bool placeable = true;
     
+    //If we don't track all collisions with a list, the tower will become placeable on trigger exit even though there was a second collision.
+    private List<Collider2D> collisions = new List<Collider2D>();
+    
     public GameObject towerPrefab;
     public Tower towerStats;
 
@@ -17,6 +20,7 @@ public class PlaceTowerPrefab : MonoBehaviour
     private void Start()
     {
         rangeIndicator.transform.localScale = new Vector3(towerStats.range * 2, towerStats.range * 2, 0);
+        
         spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
         foreach(SpriteRenderer spriteRenderer in spriteRenderers)
         {
@@ -35,10 +39,17 @@ public class PlaceTowerPrefab : MonoBehaviour
     {
         if(other.CompareTag("hitbox"))
         {
-            foreach(SpriteRenderer spriteRenderer in spriteRenderers)
+            //Remove the collision from the list of collisions.
+            if(collisions.Contains(other)) collisions.Remove(other);
+
+            //If there are no more collisions with hitboxes, the tower is placeable.
+            if(collisions.Count == 0)
             {
-                spriteRenderer.color = new Color(Color.white.r, Color.white.g, Color.white.b, 0.5f);
-                placeable = true;
+                foreach (SpriteRenderer spriteRenderer in spriteRenderers)
+                {
+                    spriteRenderer.color = new Color(Color.white.r, Color.white.g, Color.white.b, 0.5f);
+                    placeable = true;
+                }
             }
         }
     }
@@ -50,8 +61,12 @@ public class PlaceTowerPrefab : MonoBehaviour
             foreach (SpriteRenderer spriteRenderer in spriteRenderers)
             {
                 spriteRenderer.color = new Color(Color.red.r, Color.red.g, Color.red.b, 0.5f);
-                placeable = false;
+                
             }
+            placeable = false;
+            
+            //If this is a new collision, add it to the list of collisions.
+            if(!collisions.Contains(other)) collisions.Add(other);
         }
     }
 }
