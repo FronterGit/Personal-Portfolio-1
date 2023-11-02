a2947b0541a365bc349e044ac798f377a0233379
Fronter <mathijs.klingeman@gmail.com>
2023-11-02 17:58:25 +0100

    enemies now have a healthbar

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -3,19 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using EventBus;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float speed = 4f;
-    [SerializeField] private int health = 5;
+    [FormerlySerializedAs("health")] [SerializeField] private float maxHealth = 5;
+    private float health;
     [SerializeField] private int goldDrop = 1;
 
     [SerializeField] private Transform[] waypoints;
     [SerializeField] private int waypointIndex = 0;
-    [SerializeField] float routeProgress = 0f;
+    [SerializeField] Image healthBar;
 
     private void Start()
     {
+        health = maxHealth;
         transform.position = new Vector3(waypoints[waypointIndex].position.x, waypoints[waypointIndex].position.y, 0);
     }
 
@@ -33,7 +37,6 @@ public class Enemy : MonoBehaviour
     private void Move()
     {
         transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].position, speed * Time.deltaTime);
-        routeProgress++;
     }
 
     private void CheckPos()
@@ -55,6 +58,8 @@ public class Enemy : MonoBehaviour
         {
             Death(false );
         }
+
+        healthBar.fillAmount = (health / maxHealth);
     }
 
     protected virtual void Death(bool reachedEnd)
