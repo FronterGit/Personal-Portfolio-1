3e0485a94fc163a29a86df0adc74190f26b8ba2a
merlijn <merlijnvan11@gmail.com>
2023-11-15 14:05:53 +0100

    towrr placements and stuff

--- Assets/Scripts/Towers/PlaceTowerPrefab.cs
+++ Assets/Scripts/Towers/PlaceTowerPrefab.cs
@@ -7,6 +7,7 @@ using UnityEngine.Serialization;
 public class PlaceTowerPrefab : MonoBehaviour {
     private SpriteRenderer[] spriteRenderers;
     public bool placeable;
+    public bool waterUnit = false;
 
     //If we don't track all collisions with a list, the tower will become placeable on trigger exit even though there was a second collision.
     [SerializeField] private List<Collider2D> collisions = new List<Collider2D>();
@@ -31,30 +32,56 @@ public class PlaceTowerPrefab : MonoBehaviour {
     }
 
     private void OnTriggerExit2D(Collider2D other) {
-        if (other.CompareTag("hitbox")) {
-            //Remove the collision from the list of collisions.
-            if (collisions.Contains(other)) collisions.Remove(other);
-
-            //If there are no more collisions with hitboxes, the tower is placeable.
-            if (collisions.Count == 0) {
+        if (waterUnit) {
+            if (other.CompareTag("water")) {
                 foreach (SpriteRenderer spriteRenderer in spriteRenderers) {
-                    spriteRenderer.color = new Color(Color.white.r, Color.white.g, Color.white.b, 0.5f);
-                    placeable = true;
+                    spriteRenderer.color = new Color(Color.red.r, Color.red.g, Color.red.b, 0.5f);
+                }
+
+                placeable = false;
+
+                //If this is a new collision, add it to the list of collisions.
+                if (!collisions.Contains(other)) collisions.Add(other);
+            }
+        } else {
+            if (other.CompareTag("hitbox") || other.CompareTag("water")) {
+                //Remove the collision from the list of collisions.
+                if (collisions.Contains(other)) collisions.Remove(other);
+
+                //If there are no more collisions with hitboxes, the tower is placeable.
+                if (collisions.Count == 0) {
+                    foreach (SpriteRenderer spriteRenderer in spriteRenderers) {
+                        spriteRenderer.color = new Color(Color.white.r, Color.white.g, Color.white.b, 0.5f);
+                        placeable = true;
+                    }
                 }
             }
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.CompareTag("hitbox")) {
-            foreach (SpriteRenderer spriteRenderer in spriteRenderers) {
-                spriteRenderer.color = new Color(Color.red.r, Color.red.g, Color.red.b, 0.5f);
+        if (waterUnit) {
+            if (other.CompareTag("water")) {
+                foreach (SpriteRenderer spriteRenderer in spriteRenderers) {
+                    spriteRenderer.color = new Color(Color.white.r, Color.white.g, Color.white.b, 0.5f);
+                }
+
+                placeable = true;
+
+                //If this is a new collision, add it to the list of collisions.
+                if (!collisions.Contains(other)) collisions.Add(other);
             }
+        } else {
+            if (other.CompareTag("hitbox") || other.CompareTag("water")) {
+                foreach (SpriteRenderer spriteRenderer in spriteRenderers) {
+                    spriteRenderer.color = new Color(Color.red.r, Color.red.g, Color.red.b, 0.5f);
+                }
 
-            placeable = false;
+                placeable = false;
 
-            //If this is a new collision, add it to the list of collisions.
-            if (!collisions.Contains(other)) collisions.Add(other);
+                //If this is a new collision, add it to the list of collisions.
+                if (!collisions.Contains(other)) collisions.Add(other);
+            }
         }
     }
 }
