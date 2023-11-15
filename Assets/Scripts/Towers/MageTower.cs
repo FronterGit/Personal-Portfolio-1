16d688085a2dff485515e20bf06488402efea15e
merlijn <merlijnvan11@gmail.com>
2023-11-15 13:55:42 +0100

    tower fixes

--- Assets/Scripts/Towers/MageTower.cs
+++ Assets/Scripts/Towers/MageTower.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -15,6 +16,37 @@ public class MageTower : Tower {
                 attacked.Add(enemiesInRange[i]);
                 enemiesInRange[i].TakeDamage(damage);
             }
+        }
+    }
+
+    protected override IEnumerator FireCooldown(float wait) {
+        //If there are enemies in range...
+        if (enemiesInRange.Count > 0) {
+            //...set the target to the first enemy in range,
+            SetTarget(enemiesInRange);
+
+            if (target != null) {
+                Attack();
+            }
+
+            toRemoveAtEndOfFrame.ForEach(e => enemiesInRange.Remove(e));
+            toRemoveAtEndOfFrame.Clear();
+
+            //Wait for the fireRate before shooting again
+            yield return new WaitForSeconds(wait);
+            StartCoroutine(FireCooldown(internalAttackSpeed));
+        } else yield return null;
+    }
+
+    protected override void OnTriggerExit2D(Collider2D collision) {
+        //If an enemy leaves the tower's range, remove it from the list of enemies in range
+        if (collision.CompareTag("Enemy")) {
+            // enemiesInRange.Remove(collision.GetComponent<Enemy>());
+
+            toRemoveAtEndOfFrame.Add(collision.GetComponent<Enemy>());
+
+            //If there are no more enemies in range, stop shooting
+            if (enemiesInRange.Count == 0) StopAllCoroutines();
         }
     }
 }
