0da6449480eed59ee4c637a74379aad9d2dbab63
merlijn <merlijnvan11@gmail.com>
2023-11-15 13:37:53 +0100

    liol

--- Assets/Scripts/Towers/PlaceTowerPrefab.cs
+++ Assets/Scripts/Towers/PlaceTowerPrefab.cs
@@ -4,49 +4,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class PlaceTowerPrefab : MonoBehaviour
-{
+public class PlaceTowerPrefab : MonoBehaviour {
     private SpriteRenderer[] spriteRenderers;
     public bool placeable;
-    
+
     //If we don't track all collisions with a list, the tower will become placeable on trigger exit even though there was a second collision.
-    private List<Collider2D> collisions = new List<Collider2D>();
-    
+    [SerializeField] private List<Collider2D> collisions = new List<Collider2D>();
+
     public GameObject towerPrefab;
     public Tower towerStats;
 
     public GameObject rangeIndicator;
 
-    private void Start()
-    {
+    private void Start() {
         rangeIndicator.transform.localScale = new Vector3(towerStats.range * 2, towerStats.range * 2, 0);
-        
+
         spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
-        foreach(SpriteRenderer spriteRenderer in spriteRenderers)
-        {
+        foreach (SpriteRenderer spriteRenderer in spriteRenderers) {
             spriteRenderer.color = new Color(Color.red.r, Color.red.g, Color.red.b, 0.5f);
         }
-
     }
 
-    void Update()
-    {
+    void Update() {
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector3(mousePos.x, mousePos.y, 0);
     }
 
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if(other.CompareTag("hitbox"))
-        {
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.CompareTag("hitbox")) {
             //Remove the collision from the list of collisions.
-            if(collisions.Contains(other)) collisions.Remove(other);
+            if (collisions.Contains(other)) collisions.Remove(other);
 
             //If there are no more collisions with hitboxes, the tower is placeable.
-            if(collisions.Count == 0)
-            {
-                foreach (SpriteRenderer spriteRenderer in spriteRenderers)
-                {
+            if (collisions.Count == 0) {
+                foreach (SpriteRenderer spriteRenderer in spriteRenderers) {
                     spriteRenderer.color = new Color(Color.white.r, Color.white.g, Color.white.b, 0.5f);
                     placeable = true;
                 }
@@ -54,18 +45,16 @@ public class PlaceTowerPrefab : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("hitbox"))
-        {
-            foreach (SpriteRenderer spriteRenderer in spriteRenderers)
-            {
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("hitbox")) {
+            foreach (SpriteRenderer spriteRenderer in spriteRenderers) {
                 spriteRenderer.color = new Color(Color.red.r, Color.red.g, Color.red.b, 0.5f);
             }
+
             placeable = false;
-            
+
             //If this is a new collision, add it to the list of collisions.
-            if(!collisions.Contains(other)) collisions.Add(other);
+            if (!collisions.Contains(other)) collisions.Add(other);
         }
     }
 }
--- Assets/Scripts/Towers/SniperTower.cs
+++ Assets/Scripts/Towers/SniperTower.cs
@@ -9,44 +9,9 @@ public class SniperTower : Tower {
         target = enemies[0].transform;
     }
 
-    protected override void OnEnable() {
-        base.OnEnable();
-
-        EnemyManager.GetInstanceFunc().onFirstEnemySpawned += onFirstEnemySpawned;
-    }
-
-    protected override void OnDisable() {
-        base.OnDisable();
-
-        EnemyManager.GetInstanceFunc().onFirstEnemySpawned -= onFirstEnemySpawned;
-    }
-
 
     public override void Attack() {
         Bullet bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity).GetComponent<Bullet>();
         bullet.InitializeBullet(bulletSpeed, damage, target.gameObject);
-    }
-
-    protected override IEnumerator FireCooldown(float wait) {
-        List<Enemy> globalEnemies = EnemyManager.getEnemiesFunc.Invoke();
-
-
-        //If there are enemies in range...
-        if (globalEnemies.Count > 0) {
-            SetTarget(globalEnemies);
-
-            if (target != null) {
-                Attack();
-            }
-
-            yield return new WaitForSeconds(wait);
-            StartCoroutine(FireCooldown(internalAttackSpeed));
-        }
-
-        yield return null;
-    }
-
-    private void onFirstEnemySpawned() {
-        StartCoroutine(FireCooldown(internalAttackSpeed));
     }
 }
